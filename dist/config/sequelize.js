85a71b7946c0b874853486b719915500e8efe825
sherzod-uralov <sherzoduralov432@gmail.com>
2023-11-03 04:37:59 +0500

    asdasd

--- dist/config/sequelize.js
+++ dist/config/sequelize.js
@@ -2,12 +2,15 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.newSequlize = void 0;
 const sequelize_1 = require("sequelize");
-const newSequlize = new sequelize_1.Sequelize({
-    password: 'sherzod',
-    database: 'microsoft_todo',
+const newSequlize = new sequelize_1.Sequelize('postgres://microsoft_user:Uy7cdmeZuKuCM19AHiiUHsFOYEfIGSwo@dpg-cl23460310os73e1eao0-a.oregon-postgres.render.com/microsoft', {
     dialect: 'postgres',
+    protocol: 'postgres',
     logging: false,
-    username: 'postgres',
-    host: 'localhost'
+    dialectOptions: {
+        ssl: {
+            require: true,
+            rejectUnauthorized: false
+        }
+    }
 });
 exports.newSequlize = newSequlize;
--- src/config/sequelize.ts
+++ src/config/sequelize.ts
@@ -1,12 +1,15 @@
 import { Sequelize } from "sequelize";
 
-const newSequlize = new Sequelize({
-  password:'sherzod',
-  database:'microsoft_todo',
-  dialect:'postgres',
-  logging:false,
-  username:'postgres',
-  host:'localhost'
+const newSequlize = new Sequelize('postgres://microsoft_user:Uy7cdmeZuKuCM19AHiiUHsFOYEfIGSwo@dpg-cl23460310os73e1eao0-a.oregon-postgres.render.com/microsoft',{
+    dialect: 'postgres',
+    protocol: 'postgres',
+    logging:false,  
+    dialectOptions: {
+      ssl: {
+        require: true, 
+        rejectUnauthorized: false 
+      }
+    } 
 });
  
 export {newSequlize}
